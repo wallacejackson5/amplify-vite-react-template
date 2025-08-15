da2871eb2e0576329e4ef8c6110b0a5b1816c759
wallacejackson5 <36014284+wallacejackson5@users.noreply.github.com>
2025-08-15 11:29:40 -0300

    Added authentication
    * added authenticator
    
    ---------
    
    Co-authored-by: Wallace Jackson <wallace.magalhaes-ext@ab-inbev.com>

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,5 @@
 import { defineAuth } from '@aws-amplify/backend';
+import { postConfirmation } from "./post-confirmation/resource"
 
 /**
  * Define and configure your auth resource
@@ -8,4 +9,41 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  userAttributes: {
+    // Maps to Cognito standard attribute 'birthdate'
+    birthdate: {
+      mutable: true,
+      required: true,
+    },
+    // Maps to Cognito standard attribute 'email'
+    email: {
+      mutable: true,
+      required: true,
+    },
+    // Maps to Cognito standard attribute 'family_name'
+    familyName: {
+      mutable: true,
+      required: false,
+    },
+    // Maps to Cognito standard attribute 'gender'
+    gender: {
+      mutable: true,
+      required: true,
+    },
+    // Maps to Cognito standard attribute 'given_name'
+    givenName: {
+      mutable: true,
+      required: true,
+    },
+    // Maps to Cognito standard attribute 'phone_number'
+    phoneNumber: {
+      mutable: true,
+      required: false,
+    },
+  },
+  groups: ['USERS', 'ADMINS', 'PREMIUM_USERS'],
+  triggers: {
+    postConfirmation,
+  },
+  access: (allow) => [allow.resource(postConfirmation).to(["addUserToGroup"])],
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { postConfirmation } from "../auth/post-confirmation/resource";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try

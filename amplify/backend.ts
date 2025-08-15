1adf3936473893ba3c612c7ab6460d59b02b0d0a
Wallace Jackson <wallace.magalhaes-ext@ab-inbev.com>
2025-08-15 11:07:59 -0300

    added authenticator

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,13 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { postConfirmation } from './auth/post-confirmation/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+// Grant the post-confirmation function access to the data resource
+backend.data.resources.graphqlApi.grantQuery(backend.auth.resources.userPoolPostConfirmationTrigger, "UserProfile");
+backend.data.resources.graphqlApi.grantMutation(backend.auth.resources.userPoolPostConfirmationTrigger, "UserProfile");

47afd274bbc01104e5e328c996d6ba6bcd48b48c
Wallace Jackson <wallace.magalhaes-ext@ab-inbev.com>
2025-08-15 23:28:03 -0300

    added authenticator

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -4,7 +4,7 @@ export const postConfirmation = defineFunction({
   name: 'post-confirmation',
   // Default group for new users - matches the groups defined in auth config
   environment: {
-    GROUP_NAME: 'USERS'
+    GROUP_NAME: 'BASIC'
   },
   // Timeout increased to accommodate database operations
   timeoutSeconds: 30
